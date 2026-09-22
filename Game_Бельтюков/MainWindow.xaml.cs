c4a4b3bc09146f1f3d47861739592d52a9e7f361
саша <@HOME-PC>
2026-09-22 14:34:24 +0500

    Создание таймера и метода AttackPlayer. Бельтюков.

--- Game_Бельтюков/MainWindow.xaml.cs
+++ Game_Бельтюков/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Game_Бельтюков
 {
@@ -21,10 +22,18 @@ namespace Game_Бельтюков
     public partial class MainWindow : Window
     {
         public Classes.PersonInfo Player = new Classes.PersonInfo("Student", 100, 10, 1, 0, 0, 5);
+        public List<Classes.PersonInfo> Enemys = new List<Classes.PersonInfo>();
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
             UserInfoPlayer();
+            Enemys.Add(new Classes.PersonInfo("Название врага №1", 100, 20, 1, 15, 5, 20));
+            Enemys.Add(new Classes.PersonInfo("Название врага №2", 20, 5, 1, 5, 2, 5));
+            Enemys.Add(new Classes.PersonInfo("Название врага №3", 50, 3, 1, 10, 10, 15));
+            dispatcherTimer.Tick += AttackPlayer;
+            dispatcherTimer.Interval = new System.TimeSpan(0, 0, 10);
+            dispatcherTimer.Start();
         }
         public void UserInfoPlayer()
         {
@@ -32,7 +41,7 @@ namespace Game_Бельтюков
             {
                 Player.Level++;
                 Player.Glasses = 0;
-                Player.Health += 100;
+                Player.Health += 100; 
                 Player.Damage++;
                 Player.Armor++;
             }
@@ -41,6 +50,10 @@ namespace Game_Бельтюков
             playerLevel.Content = "Уровень: " + Player.Level;
             playerGlasses.Content = "Опыт: " + Player.Glasses;
             playerMoney.Content = "Монеты: " + Player.Money;
+        }
+        private void AttackPlayer(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
